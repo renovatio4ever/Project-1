35f9d9732aa395425e46328eb8b625fe18d99f19
nanohurtz <renovatio4ever@gmail.com>
2018-11-10 13:58:27 -0500

    Latest FE Build

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -7,6 +7,7 @@
 // Section also validates that data (is at least) reaching the database
 
 // Initialize Firebase
+
 var config = {
     apiKey: "AIzaSyC19BuOOU7CeeMF_O1V8s0M3xMx-MyoLHI",
     authDomain: "dream-team-game.firebaseapp.com",
@@ -37,39 +38,42 @@ var tokensearned = 0
 var totalearnings = 0
 var betlocker = "false"
 var currentplayer
+var gameplayer
 
-// function setuser() {
-//     currentplayer = $("#new-player").text();
-//     // currentplayer = "Player9999"
-//     console.log(currentplayer);
-//     $("#playing-as").text(currentplayer);
-// }
+function GenerateNFLTeams(){
+    
+    var queryURL = "https://api.fantasydata.net/v3/nfl/scores/JSON/GameStatsByWeek/2018/8?key=ad398993c55d46449bde67a4095fef1b";
 
-// setuser();
+    console.log(queryURL); 
+    $.ajax({
+        url: queryURL,
+        method: 'GET'
+    })
+    .done(function(response) {
+        // var results = response.data; 
+        console.log(response)
+        // for (var i=0; i<results.length; i++){
 
-// Test Harness: Initialization
+        // }
+    });
+}
 
-
+GenerateNFLTeams();
 
 $(document).ready(function () {
 
     // simplemask.js masking.. because fat fingering text will goof the generator.
-    
-    $('#wins-1').simpleMask({
-        'mask': ['###'],
-        'nextInput': $('#loss-1')
-    });
 
-    $('#loss-1').simpleMask({
-        'mask': ['###'],
-        'nextInput': $('#draw-1')
-    });
-  
-    $('#draw-1').simpleMask({
-        'mask': ['###'],
-        'nextInput': $('#wins-1')
-    });
- 
+    $('#wins-1').simpleMask({'mask': ['###'],'nextInput': $('#loss-1')});
+    $('#loss-1').simpleMask({'mask': ['###'],'nextInput': $('#draw-1')});
+    $('#draw-1').simpleMask({'mask': ['###'],'nextInput': $('#wins-2')});
+    $('#wins-2').simpleMask({'mask': ['###'],'nextInput': $('#loss-2')});
+    $('#loss-2').simpleMask({'mask': ['###'],'nextInput': $('#draw-2')});
+    $('#draw-2').simpleMask({'mask': ['###'],'nextInput': $('#wins-3')});
+    $('#wins-3').simpleMask({'mask': ['###'],'nextInput': $('#loss-3')});
+    $('#loss-3').simpleMask({'mask': ['###'],'nextInput': $('#draw-3')});
+    $('#draw-3').simpleMask({'mask': ['###'],'nextInput': $('#wins-1')});
+
 });
 
 function hidethestack() {
@@ -90,28 +94,39 @@ $('input').keyup(function () {
 // Test Harness: Interim Bet Engine
 function calcbets() {
 
+
+    // $("tr").each(function() {
+    //     if ($(this).find(".win")) {
+    //       team1earn = parseInt($(this).find(".win").val().trim())
+    //       team2earn = parseInt($(this).find(".loss").val().trim())
+    //       team3earn = parseInt($(this).find(".draw").val().trim())
+    //     }
+    //   })
+    
     wins1 = $("#wins-1").val().trim();
     loss1 = $("#loss-1").val().trim();
     draw1 = $("#draw-1").val().trim();
 
+    wins2 = $("#wins-2").val().trim();
+    loss2 = $("#loss-2").val().trim();
+    draw2 = $("#draw-2").val().trim();
 
-    // console.log(wins1 + " " + loss1 + " " + draw1);
+    wins3 = $("#wins-3").val().trim();
+    loss3 = $("#loss-3").val().trim();
+    draw3 = $("#draw-3").val().trim();
 
-    // team1earn = ((wins1 + loss1 + draw1) * odds1);
-    //If stats change do this: ((wins + loss + draw)* odds)
     team1earn = parseInt(wins1) + parseInt(loss1) + parseInt(draw1);
-  
-
-    // console.log("Team Earnings " + team1earn)
-
-    totalbets = team1earn
-    // console.log("Total Bets " + totalbets)
+    team2earn = parseInt(wins2) + parseInt(loss2) + parseInt(draw2);
+    team3earn = parseInt(wins3) + parseInt(loss3) + parseInt(draw3);
+    
+    totalbets = team1earn + team2earn + team3earn
 
     tokensearned = totaltokens - totalbets
-    // console.log("Total Tokens Earned " + tokensearned)
 
     $("#earn-1").text(team1earn);
- 
+    $("#earn-2").text(team2earn);
+    $("#earn-3").text(team3earn);
+
     $("#totalearnings").text(totalbets);
     $("#tokens-earned").text(tokensearned);
 
@@ -119,12 +134,15 @@ function calcbets() {
     database.ref().update({
         t_token: tokensearned,
         t1_earn: team1earn,
+        t2_earn: team2earn,
+        t3_earn: team3earn,
         t_earnings: totalbets
     });
 }
 
 function overbet() {
     if (tokensearned < 1) {
+        $("#mymodal-youwentover").modal('show');
         $(':input').val(0);
 
         $("#earn-1").text(0);
@@ -146,12 +164,36 @@ $("#bet-this").click(function () {
     });
 });
 
+$("#pick-team-1").attr("disabled", true);
 
 $("#go-bet-now").click(function () {
-    $("#hide-the-welcome").hide();
-    $("#hide-the-team").show();
-    $("#hide-the-bets").hide();
-    $("#hide-the-champs").hide();
+
+    // ref.child('dream-team-game').orderByChild('player').equalTo('superplayer2018').on("value", function(snapshot) {
+    //     console.log(snapshot.val());
+    //     snapshot.forEach(function(data) {
+    //         console.log(data.key);
+    //     });
+    // });
+
+    $("#game-player").on("keyup change", function () {
+        gameplayer = this.value;
+    });
+
+    if (gameplayer === "superplayer2018") {
+        $("#mymodal-existing").modal('show');
+        $("#hide-the-welcome").hide();
+        $("#hide-the-team").hide();
+        $("#hide-the-bets").show();
+        $("#hide-the-champs").hide();
+    } else {
+        $("#mymodal-register").modal('show');
+        $("#hide-the-welcome").hide();
+        $("#hide-the-team").show();
+        $("#hide-the-bets").hide();
+        $("#hide-the-champs").hide();
+
+    }
+
 });
 
 $("#go-to-champs").click(function () {
@@ -161,19 +203,42 @@ $("#go-to-champs").click(function () {
     $("#hide-the-champs").show();
 });
 
+// Disable Button Count
 $("#pick-team-1").click(function () {
-    $("#hide-the-welcome").hide();
-    $("#hide-the-team").hide();
-    $("#hide-the-bets").show();
-    $("#hide-the-champs").hide();
+    $("#pick-team-1").attr("disabled", true);
 });
 
+$("#pick-team-2").click(function () {
+    $("#pick-team-2").attr("disabled", true);
+});
+
+$("#pick-team-3").click(function () {
+    $("#pick-team-3").attr("disabled", true);
+});
+
+$("#pick-team-4").click(function () {
+    $("#pick-team-4").attr("disabled", true);
+});
+
+$("#pick-team-5").click(function () {
+    $("#pick-team-5").attr("disabled", true);
+});
+
+// End Series
 $("#go-back-to-teams").click(function () {
     $("#hide-the-welcome").hide();
     $("#hide-the-team").show();
     $("#hide-the-bets").hide();
     $("#hide-the-champs").hide();
 });
+
+$("#go-to-bet-board").click(function () {
+    $("#hide-the-welcome").hide();
+    $("#hide-the-team").hide();
+    $("#hide-the-bets").show();
+    $("#hide-the-champs").hide();
+});
+
 
 $("#go-to-home").click(function () {
     $("#hide-the-welcome").show();
